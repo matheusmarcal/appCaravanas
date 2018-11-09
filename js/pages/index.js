9b3eeec1c0141a0d4b094b7f1c76b1f93db8b9d5
Ítalo Cássio <italocassio95@gmail.com>
2018-11-09 01:40:04 -0200

    buscando eventos
    adicionando busca dos eventos direto da api

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,17 +1,5 @@
 ﻿$(function () {
-    //Widgets count
-    $('.count-to').countTo();
-
-    //Sales count to
-    $('.sales-count-to').countTo({
-        formatter: function (value, options) {
-            return '$' + value.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, ' ').replace('.', ',');
-        }
-    });
-
-    //initRealTimeChart();
-    //initDonutChart();
-    //initSparkline();
+    
 });
 
 var realtime = 'on';
--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
-﻿
+﻿$(function () {
+     alert('ola')
+		carregarEventos()
+	
+		});
+		
+		function irPara(tela){
+
+			$(".clearfix").hide();
+			$("#div_"+ tela).show();
+
+		}
+		
+		
+function carregarEventos() {
+    alert('car eve');
+    $.ajax({
+        type: "GET",
+        url: "https://querocaravanasapi.azurewebsites.net/api/Evento",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: {},
+        beforeSend: function () {
+           //load
+        },
+        success: function (response) {           
+            var d = response;
+			console.log(d);
+            var html = "";
+            d.forEach(item => {
+                html += `<div class="card">
+                          <div class="body">
+							<b style="font-size:20px;">${item.titulo}</b><br>
+                            ${item.descricao}<br><br>
+							<b>Data</b>: ${item.data}<br>
+							<button type="button" onclick="irPara('detalhes')" class="btn btn-block btn-lg btn-info waves-effect">VER</button>
+                        </div>
+                      </div>`;
+            });
+            $("#divEventos").html(html).show();
+        },
+        failure: function (response) {            
+            alert('fail');
+        },
+        error: function (response) {
+             alert('error');
+        }
+    });
+}
+	
